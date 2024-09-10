734fa38b23a102453a33a72e0a45539913b17f30
Abel731 <abelp9289@gmail.com>
2024-09-09 20:37:08 -0400

    probando  las rutas de la referencial ciudad

--- app/rutas/referenciales/ciudad/ciudad_routes.py
+++ app/rutas/referenciales/ciudad/ciudad_routes.py
@@ -1,7 +1,34 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, url_for, redirect
+from app.dao.referenciales.ciudad.CiudadDao import CiudadDao
 
 ciumod = Blueprint('ciudad', __name__, template_folder='templates')
 
+ciumod.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 @ciumod.route('/ciudad-index')
 def ciudadIndex():
-    return render_template('ciudad-index.html')
+    ciudadDao = CiudadDao()
+    return render_template('ciudad-index.html', lista_ciudades=ciudadDao.getCiudades())
+
+@ciumod.route('/ciudad-agregar')
+def ciudadAgregar():
+    return render_template('ciudad-agregar.html')
+
+@ciumod.route('/guardar-ciudad', methods=['POST'])
+def guardarCiudad():
+    ciudad = request.form.get('txtDescripcion').strip()
+    if ciudad == None or len(ciudad) < 1:
+        # mostrar un mensaje al usuario
+        flash('Debe escribir algo en la descripcion', 'warning')
+
+        # redireccionar a la vista ciudades
+        return redirect(url_for('ciudadAgregar'))
+
+    ciudaddao = CiudadDao()
+    ciudaddao.guardarCiudad(ciudad.upper())
+
+    # mostrar un mensaje al usuario
+    flash('Guardado exitoso', 'success')
+
+    # redireccionar a la vista ciudades
+    return redirect(url_for('ciudadIndex'))
