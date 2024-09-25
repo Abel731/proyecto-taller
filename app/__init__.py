b2b090706f681206a670344d3f6d106abd1455b2
Abel731 <abelp9289@gmail.com>
2024-09-24 20:03:42 -0400

    se creo 3 referencialesciudad,pais,nacionalidad

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,23 @@ from flask import Flask
 ## instancia para arrancar el proyecto
 app = Flask (__name__)
 
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 # importar referenciales
 from app.rutas.referenciales.ciudad.ciudad_routes import ciumod
-
+from app.rutas.referenciales.pais.pais_routes import paismod
+from app.rutas.referenciales.nacionalidad.nacionalidad_routes import nacmod
 # registrar referenciales 
 modulo0 = '/referenciales'
 app.register_blueprint(ciumod, url_prefix=f'{modulo0}/ciudad')
+app.register_blueprint(paismod, url_prefix=f'{modulo0}/pais')
+app.register_blueprint(nacmod, url_prefix=f'{modulo0}/nacionalidad')
+# importar APIS v1
+from app.rutas.referenciales.ciudad.ciudad_api import ciuapi
+from app.rutas.referenciales.pais.pais_api import paiapi
+from app.rutas.referenciales.nacionalidad.nacionalidad_api import nacapi
+# registrar APIS
+version1 = '/api/v1'
+app.register_blueprint(ciuapi, url_prefix=version1)
+app.register_blueprint(paiapi, url_prefix=version1)
+app.register_blueprint(nacapi, url_prefix=version1)
--- app/codigos_sql/referenciales.sql
+++ app/codigos_sql/referenciales.sql
@@ -6,3 +6,13 @@ CREATE TABLE ciudades (
 
 SELECT id, descripcion 
 FROM ciudades;
+
+CREATE TABLE paises (
+    id SERIAL PRIMARY KEY,
+    descripcion VARCHAR(60) UNIQUE
+);
+
+CREATE TABLE nacionalidades (
+    id SERIAL PRIMARY KEY,
+    descripcion VARCHAR(60) UNIQUE
+);
