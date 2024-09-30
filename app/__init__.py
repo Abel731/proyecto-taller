aa73cba6646a2f9465472150e60ae300e839658e
Abel731 <abelp9289@gmail.com>
2024-09-30 16:10:34 -0400

    nueva referencial, se puede mejorar, referencial de proveedor puede haber campos que tienen que ser boolean pero asi me salio despues de roperme la cabeza :)

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from app.rutas.referenciales.pais.pais_routes import paismod
 from app.rutas.referenciales.nacionalidad.nacionalidad_routes import nacmod
 from app.rutas.referenciales.producto.producto_routes import promod
 from app.rutas.referenciales.persona.persona_routes import permod
+from app.rutas.referenciales.proveedor.proveedor_routes import provmod
 # registrar referenciales 
 modulo0 = '/referenciales'
 app.register_blueprint(ciumod, url_prefix=f'{modulo0}/ciudad')
@@ -17,12 +18,14 @@ app.register_blueprint(paismod, url_prefix=f'{modulo0}/pais')
 app.register_blueprint(nacmod, url_prefix=f'{modulo0}/nacionalidad')
 app.register_blueprint(promod, url_prefix=f'{modulo0}/producto')
 app.register_blueprint(permod, url_prefix=f'{modulo0}/persona')
+app.register_blueprint(provmod, url_prefix=f'{modulo0}/proveedor')
 # importar APIS v1
 from app.rutas.referenciales.ciudad.ciudad_api import ciuapi
 from app.rutas.referenciales.pais.pais_api import paiapi
 from app.rutas.referenciales.nacionalidad.nacionalidad_api import nacapi
 from app.rutas.referenciales.producto.producto_api import proapi
 from app.rutas.referenciales.persona.persona_api import perapi
+from app.rutas.referenciales.proveedor.proveedor_api import provapi
 # registrar APIS
 version1 = '/api/v1'
 app.register_blueprint(ciuapi, url_prefix=version1)
@@ -30,3 +33,4 @@ app.register_blueprint(paiapi, url_prefix=version1)
 app.register_blueprint(nacapi, url_prefix=version1)
 app.register_blueprint(proapi, url_prefix=version1)
 app.register_blueprint(perapi, url_prefix=version1)
+app.register_blueprint(provapi, url_prefix=version1)
