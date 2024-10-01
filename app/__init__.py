efed2c597fc6d6579e63981892acfb129e469f99
Abel731 <abelp9289@gmail.com>
2024-09-30 22:33:33 -0400

    me costo pero ya otra referencial y es de cliente :)

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,7 @@ from app.rutas.referenciales.nacionalidad.nacionalidad_routes import nacmod
 from app.rutas.referenciales.producto.producto_routes import promod
 from app.rutas.referenciales.persona.persona_routes import permod
 from app.rutas.referenciales.proveedor.proveedor_routes import provmod
+from app.rutas.referenciales.cliente.cliente_routes import climod
 # registrar referenciales 
 modulo0 = '/referenciales'
 app.register_blueprint(ciumod, url_prefix=f'{modulo0}/ciudad')
@@ -19,6 +20,7 @@ app.register_blueprint(nacmod, url_prefix=f'{modulo0}/nacionalidad')
 app.register_blueprint(promod, url_prefix=f'{modulo0}/producto')
 app.register_blueprint(permod, url_prefix=f'{modulo0}/persona')
 app.register_blueprint(provmod, url_prefix=f'{modulo0}/proveedor')
+app.register_blueprint(climod, url_prefix=f'{modulo0}/cliente')
 # importar APIS v1
 from app.rutas.referenciales.ciudad.ciudad_api import ciuapi
 from app.rutas.referenciales.pais.pais_api import paiapi
@@ -26,6 +28,7 @@ from app.rutas.referenciales.nacionalidad.nacionalidad_api import nacapi
 from app.rutas.referenciales.producto.producto_api import proapi
 from app.rutas.referenciales.persona.persona_api import perapi
 from app.rutas.referenciales.proveedor.proveedor_api import provapi
+from app.rutas.referenciales.cliente.cliente_api import cliapi
 # registrar APIS
 version1 = '/api/v1'
 app.register_blueprint(ciuapi, url_prefix=version1)
@@ -34,3 +37,4 @@ app.register_blueprint(nacapi, url_prefix=version1)
 app.register_blueprint(proapi, url_prefix=version1)
 app.register_blueprint(perapi, url_prefix=version1)
 app.register_blueprint(provapi, url_prefix=version1)
+app.register_blueprint(cliapi, url_prefix=version1)
