780ba6a26c12cd15553bf7fbffc31a8749cf82a8
Abel731 <abelp9289@gmail.com>
2024-10-04 21:34:51 -0400

    se creo el crud de sexo

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,7 @@ from app.rutas.referenciales.sucursal.sucursal_routes import sucmod
 from app.rutas.referenciales.deposito.deposito_routes import depomod
 from app.rutas.referenciales.cargo.cargo_routes import carmod
 from app.rutas.referenciales.estado_civil.estado_civil_routes import estmod
+from app.rutas.referenciales.sexo.sexo_routes import sexomod
 # registrar referenciales 
 modulo0 = '/referenciales'
 app.register_blueprint(ciumod, url_prefix=f'{modulo0}/ciudad')
@@ -29,6 +30,7 @@ app.register_blueprint(sucmod, url_prefix=f'{modulo0}/sucursal')
 app.register_blueprint(depomod, url_prefix=f'{modulo0}/deposito')
 app.register_blueprint(carmod, url_prefix=f'{modulo0}/cargo')
 app.register_blueprint(estmod, url_prefix=f'{modulo0}/estado_civil')
+app.register_blueprint(sexomod, url_prefix=f'{modulo0}/sexo')
 # importar APIS v1
 from app.rutas.referenciales.ciudad.ciudad_api import ciuapi
 from app.rutas.referenciales.pais.pais_api import paiapi
@@ -41,6 +43,7 @@ from app.rutas.referenciales.sucursal.sucursal_api import sucapi
 from app.rutas.referenciales.deposito.deposito_api import depoapi
 from app.rutas.referenciales.cargo.cargo_api import carapi
 from app.rutas.referenciales.estado_civil.estado_civil_api import estadocivilapi
+from app.rutas.referenciales.sexo.sexo_api import sexoapi
 # registrar APIS
 version1 = '/api/v1'
 app.register_blueprint(ciuapi, url_prefix=version1)
@@ -54,3 +57,4 @@ app.register_blueprint(sucapi, url_prefix=version1)
 app.register_blueprint(depoapi, url_prefix=version1)
 app.register_blueprint(carapi, url_prefix=version1)
 app.register_blueprint(estadocivilapi, url_prefix=version1)
+app.register_blueprint(sexoapi, url_prefix=version1)
