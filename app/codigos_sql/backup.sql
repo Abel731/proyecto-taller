2d921e98534610ab00c457e134962377cc1b84fc
Abel731 <abelp9289@gmail.com>
2024-12-22 21:59:13 -0300

    codigos sql para tener guardado para wl futuro

---
+++ app/codigos_sql/backup.sql
@@ -0,0 +1,190 @@
+CREATE TABLE estado_de_pedido_compras(
+    id_epc SERIAL PRIMARY KEY,
+    descripcion VARCHAR(60) UNIQUE NOT NULL
+);
+
+CREATE TABLE sucursales(
+    id_sucursal SERIAL PRIMARY KEY,
+    descripcion VARCHAR(60) UNIQUE NOT NULL
+);
+
+CREATE TABLE depositos(
+    id_deposito SERIAL PRIMARY KEY,
+    descripcion VARCHAR(60) UNIQUE NOT NULL
+);
+
+CREATE TABLE sucursal_depositos(
+    id_sucursal INTEGER NOT NULL,
+    id_deposito INTEGER NOT NULL,
+    observaciones VARCHAR(60),
+    estado BOOLEAN NOT NULL,
+    PRIMARY KEY(id_sucursal, id_deposito),
+    FOREIGN KEY(id_sucursal) REFERENCES sucursales(id_sucursal)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY(id_deposito) REFERENCES depositos(id_deposito)
+    ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE productos(
+    id_producto SERIAL PRIMARY KEY,
+    nombre VARCHAR(60) UNIQUE NOT NULL,
+    cantidad INTEGER,
+    precio_unitario DECIMAL (10,2)
+);
+
+
+CREATE TABLE personas(
+    id_persona SERIAL PRIMARY KEY,
+    nombres VARCHAR(70) NOT NULL,
+    apellidos VARCHAR(70) NOT NULL,
+    ci TEXT NOT NULL,
+    fechanac DATE,
+    creacion_fecha DATE NOT NULL,
+    creacion_hora TIME NOT NULL,
+    creacion_usuario INTEGER NOT NULL,
+    modificacion_fecha DATE,
+    modificacion_hora TIME,
+    modificacion_usuario INTEGER--,
+    /*FOREIGN KEY(creacion_usuario) REFERENCES
+    usuarios(id_usuario)
+    ON DELETE RESTRICT ON UPDATE CASCADE
+    FOREIGN KEY(modificacion_usuario) REFERENCES
+    usuarios(id_usuario)
+    ON DELETE RESTRICT ON UPDATE CASCADE*/
+);
+
+CREATE TABLE empleados(
+    id_empleado INTEGER PRIMARY KEY,
+    car_id INTEGER NOT null,
+    fecha_ingreso DATE NOT NULL,
+    FOREIGN KEY(id_empleado) REFERENCES personas(id_persona)
+    ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+
+CREATE TABLE pedido_de_compra(
+    id_pedido_compra SERIAL PRIMARY KEY
+    , id_empleado INTEGER NOT NULL
+    , id_sucursal INTEGER NOT NULL
+    , id_epc INTEGER NOT NULL
+    , fecha_pedido DATE NOT NULL
+    , id_deposito INTEGER NOT NULL
+    , FOREIGN KEY(id_empleado) REFERENCES empleados(id_empleado)
+    , FOREIGN KEY(id_sucursal) REFERENCES sucursales(id_sucursal)
+    , FOREIGN KEY(id_epc) REFERENCES estado_de_pedido_compras(id_epc)
+    , FOREIGN KEY(id_deposito) REFERENCES depositos(id_deposito)
+);
+
+CREATE TABLE pedido_de_compra_detalle(
+    id_pedido_compra INTEGER NOT NULL
+    , id_producto INTEGER NOT NULL
+    , cantidad INTEGER NOT NULL
+    , PRIMARY KEY(id_pedido_compra, id_producto)
+    , FOREIGN KEY(id_pedido_compra) REFERENCES pedido_de_compra(id_pedido_compra) ON DELETE RESTRICT ON UPDATE CASCADE
+    , FOREIGN KEY(id_producto) REFERENCES productos(id_producto) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+
+CREATE TABLE proveedores(
+    id_proveedor SERIAL PRIMARY KEY
+    , ruc VARCHAR(20) UNIQUE NOT NULL
+    , razon_social VARCHAR(70) NOT NULL
+    , direccion VARCHAR(70) NOT NULL
+    , telefono VARCHAR(20) NOT NULL   
+);
+
+CREATE TABLE clientes (
+    id_cliente Integer PRIMARY key NOT NULL,   
+    direccion TEXT NOT NULL,                 
+    telefono VARCHAR(20),                   
+    FOREIGN KEY (id_cliente) REFERENCES personas(id_persona) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+SELECT 
+    c.id_cliente,
+    CONCAT(p.nombres, ' ', p.apellidos) AS cliente,
+    p.ci	
+FROM 
+    clientes c
+LEFT JOIN 
+    personas p ON c.id_cliente = p.id_persona;
+   
+   
+
+CREATE TABLE presupuesto_prov(
+    id_presupuesto SERIAL PRIMARY KEY
+    , id_proveedor INTEGER NOT NULL
+    , id_pedido_compra INTEGER NOT NULL
+    , id_empleado INTEGER NOT NULL
+    , id_sucursal INTEGER NOT NULL
+    , id_estpreprov INTEGER NOT NULL
+    , fecha_presupuesto DATE NOT null
+    , fecha_vencimiento DATE not null
+    , FOREIGN KEY(id_proveedor) REFERENCES proveedores(id_proveedor) ON DELETE RESTRICT ON UPDATE CASCADE
+    , FOREIGN KEY(id_pedido_compra) REFERENCES pedido_de_compra(id_pedido_compra) ON DELETE RESTRICT ON UPDATE CASCADE
+    , FOREIGN KEY(id_empleado) REFERENCES empleados(id_empleado) ON DELETE RESTRICT ON UPDATE CASCADE
+    , FOREIGN KEY(id_sucursal) REFERENCES sucursales(id_sucursal) ON DELETE RESTRICT ON UPDATE CASCADE
+    , FOREIGN KEY(id_estpreprov) REFERENCES estado_de_presupuesto_prov(id_estpreprov) ON DELETE RESTRICT ON UPDATE CASCADE
+      
+);
+
+CREATE TABLE presupuesto_prov_detalle(
+    id_presupuesto INTEGER NOT NULL
+    , id_producto INTEGER NOT NULL
+    , cantidad INTEGER NOT NULL
+    , precio_unitario DECIMAL (10,2)
+    , PRIMARY KEY(id_presupuesto, id_producto)
+    , FOREIGN KEY(id_presupuesto) REFERENCES presupuesto_prov(id_presupuesto) ON DELETE RESTRICT ON UPDATE CASCADE
+    , FOREIGN KEY(id_producto) REFERENCES productos(id_producto) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE estado_de_presupuesto_prov(
+    id_estpreprov SERIAL PRIMARY KEY
+    , descripcion VARCHAR  NOT NULL
+);
+
+
+SELECT
+            pdp.id_presupuesto
+            , pdp.id_pedido_compra
+            , pdp.id_empleado
+            , p.nombres
+            , p.apellidos
+            , pdp.id_sucursal
+            , pdp.id_proveedor
+            , prov.ruc
+            , prov.razon_social
+            , pdp.id_estpreprov
+            , edp.descripcion AS estado
+            , pdp.fecha_presupuesto
+            , pdp.fecha_vencimiento
+        FROM
+            public.presupuesto_prov pdp
+        LEFT JOIN proveedores prov
+            ON prov.id_proveedor = pdp.id_proveedor
+        LEFT JOIN empleados e
+            ON e.id_empleado = pdp.id_empleado
+        LEFT JOIN personas p
+            ON p.id_persona = e.id_empleado
+        LEFT JOIN estado_de_presupuesto_prov edp
+            ON edp.id_estpreprov = pdp.id_estpreprov;
+
+CREATE TABLE usuarios (
+    id_usuario SERIAL PRIMARY KEY,
+    usuario VARCHAR(50) UNIQUE NOT NULL,
+    clave VARCHAR(300) NOT NULL,
+    estado BOOLEAN DEFAULT TRUE NOT NULL
+);
+
+CREATE TABLE roles (
+    id_rol SERIAL PRIMARY KEY,
+    nombre_rol VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE usuario_roles (
+    id_usuario INTEGER NOT NULL,
+    id_rol INTEGER NOT NULL,
+    PRIMARY KEY(id_usuario, id_rol),
+    FOREIGN KEY(id_usuario) REFERENCES usuarios(id_usuario) ON DELETE CASCADE,
+    FOREIGN KEY(id_rol) REFERENCES roles(id_rol) ON DELETE CASCADE
+);
